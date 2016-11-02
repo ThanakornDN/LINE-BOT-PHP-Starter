1f6e42571a2951ab06ac5e1692c920afeeac7cf9
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-02 16:26:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,16 +16,17 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+				if ($text=='1') )
+				{
+				    $a=>"Find it";
+				}else{
+				    $a=>"Not Find it";
+				}
+			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				
-				if ($text=='1') )
-				{
-				    'text'=>"Find it";
-				}else{
-				    'text'=>"Not Find it";
-				}
+				'text' => $a
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
