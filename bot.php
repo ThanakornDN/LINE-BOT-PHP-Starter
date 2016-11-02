ccf6bad4ac418d4e5656d84339e272eea678bdf4
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-02 16:55:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 <?php
 $access_token = 'VlxSZTyumW3qJsUKMnKOTLdqRd7chFWFJARPb7ZB/n3Lzf/lntpuOwBiLNieMReH3aFrT4MoAEWCdFruNp/7VHg3RkM1ja3AUtYVlDabJUgo6wAKsQyrZVo9Vxq+/py7le7bLr6ZDSp6qQHy0RiI2gdB04t89/1O/w1cDnyilFU=';
 $a="OK1";
+$regs="";
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -16,8 +17,8 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
-			//if($text!="OK"){
-			if ( ereg ( "Hello", $text, $regs ) )
+			if($text!="OK"){
+			//if ( ereg ( "Hello", $text, $regs ) )
 				$a='ei ei';
 			}
 			
