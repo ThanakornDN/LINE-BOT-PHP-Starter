b1cfca0055d573318b321afd53e25d9ecc2cc085
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-02 16:05:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,13 +19,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				//'text' => $text
-					if ( ereg ( "สวัสดี", $text, $regs ) )
-					{
-					    'text' => "Find it's";
-					}else{
-						'text' => "Not Find it's";
-					}
+				'text' => $text
+
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
