28d1c918a2c2631cb576f4e506564d464e7bc5fc
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-03 11:28:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,12 +28,8 @@ if (!is_null($events['events'])) {
 			
 			// Build message to reply back
 			$messages = [
-				//'type' => 'text',
-				//'text' => $msg
-				'type': 'sticker',
-				'packageId': '1',
-				'stickerId': '1'
-				
+				'type' => 'text',
+				'text' => $msg
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
