e48937b58d230a64bc73a2eaf22d67d701e6d9d3
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-03 15:08:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -48,9 +48,35 @@ if (!is_null($events['events'])) {
 // 				"latitude"=>35.65910807942215,
 // 				"longitude"=>139.70372892916203
 				
-				"type"=>"sticker",
-				"packageId"=>"1",
-				"stickerId"=>"1"
+// 				"type"=>"sticker",
+// 				"packageId"=>"1",
+// 				"stickerId"=>"1"
+				
+				"type"=>"template",
+				"altText"=>"this is a buttons template",
+				"template"=>{
+				    "type"=>"buttons",
+				    "thumbnailImageUrl"=>"https://example.com/bot/images/image.jpg",
+				    "title"=>"Menu",
+				    "text"=>"Please select",
+				    "actions"=>[
+					{
+					  "type"=>"postback",
+					  "label"=>"Buy",
+					  "data"=>"action=buy&itemid=123"
+					},
+					{
+					  "type"=>"postback",
+					  "label"=>"Add to cart",
+					  "data"=>"action=add&itemid=123"
+					},
+					{
+					  "type"=>"uri",
+					  "label"=>"View detail",
+					  "uri"=>"http://example.com/page/123"
+					}
+				    ]
+				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
