7e92c805afe0523afae13e2dd21e6e0c56de4cf4
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-03 15:01:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -42,11 +42,15 @@ if (!is_null($events['events'])) {
 // 				"originalContentUrl"=>"https://example.com/original.m4a",
 // 				"duration"=>240000
 
-				"type"=>"location",
-				"title"=>"my location",
-				"address"=>"〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-				"latitude"=>35.65910807942215,
-				"longitude"=>139.70372892916203
+// 				"type"=>"location",
+// 				"title"=>"my location",
+// 				"address"=>"〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+// 				"latitude"=>35.65910807942215,
+// 				"longitude"=>139.70372892916203
+				
+				"type"=>"sticker",
+				"packageId"=>"1",
+				"stickerId"=>"1"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
