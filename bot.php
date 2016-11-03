11449e51c1d0aabec6bdc6b030e02a1824f9a835
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-03 14:30:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,8 +27,10 @@ if (!is_null($events['events'])) {
 			
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $msg
+// 				'type' => 'text',
+// 				'text' => $msg
+				 "type": "text",
+				 "text": "Hello, world"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
