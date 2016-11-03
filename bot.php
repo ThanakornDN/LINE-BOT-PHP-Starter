9961cc2ff68855a8bdc51eaa967d69b4de6684bb
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-03 15:13:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -60,7 +60,7 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
-			$post = {
+			$post = '{
 				  "type": "template",
 				  "altText": "this is a buttons template",
 				  "template": {
@@ -86,7 +86,7 @@ if (!is_null($events['events'])) {
 					  }
 				      ]
 				  }
-				}
+				}';
 			//$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
