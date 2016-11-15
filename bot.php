583e96ba5ef946895e8622e64417d24c3c2c242c
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-15 16:24:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -99,7 +99,7 @@ if (!is_null($events['events'])) {
 // 					]
 // 				];
 			$s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_ans.php?msg='.$text);
-			if($s_ans){	
+			if($s_ans!=0){	
 				$messages = [
 					'type'=>'text',
 					'text'=>$s_ans
