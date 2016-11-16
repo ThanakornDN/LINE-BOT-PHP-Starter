5d475e7d2f55822023fc4d0e0c1fc19f33c42bfc
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-16 10:12:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -99,10 +99,15 @@ if (!is_null($events['events'])) {
 // 					]
 // 				];
 			$s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_ans.php?msg='.$text);
-			if($s_ans!=""){	
+			if($s_ans!="S0"){	
 				$messages = [
 					'type'=>'text',
 					'text'=>$s_ans
+				];
+			}else if($s_ans=="S0"){
+				$messages = [
+					'type'=>'text',
+					'text'=>$text.' คืออะไรหรอครับ ?'
 				];
 			}else if((eregi ( "คือ", $text, $regs ))or(eregi ( "หมายถึง", $text, $regs ))){
 //			if((eregi ( "คือ", $text, $regs ))or(eregi ( "หมายถึง", $text, $regs ))){
