5d8ffddeba451f850e7e01c6571d27f4ce188dac
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-17 17:41:43 +0700

    Update Message.php

--- Message.php
+++ Message.php
@@ -1,19 +1,31 @@
 <?php
-// public function replyText(){
-
-// 			// Get text sent
-// 			$text = $event['message']['text'];
-// 			// Get replyToken
-// 			$replyToken = $event['replyToken'];
-
-// 			// Make a POST Request to Messaging API to reply to sender
-// 			//$url = 'https://api.line.me/v2/bot/message/reply';
-// 			$data = [
-// 				'replyToken' => $replyToken,
-// 				'messages' => $text,
-// 			];
-      
-//       return $this->$data;
-// }
-echo "OK";
-?>
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+namespace LINE\LINEBot;
+/**
+ * The interface that has a responsibility to build message.
+ *
+ * @package LINE\LINEBot
+ */
+interface MessageBuilder
+{
+    /**
+     * Builds message structure.
+     *
+     * @return array Built message structure.
+     */
+    public function buildMessage();
+}
