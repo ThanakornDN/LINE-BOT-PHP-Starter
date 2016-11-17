e94fe97f7f547b8c7178ca7ff42b64dbf2932cad
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-17 17:39:23 +0700

    Update Message.php

--- Message.php
+++ Message.php
@@ -1,19 +1,19 @@
 <?php
-public function replyText(){
+// public function replyText(){
 
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+// 			// Get text sent
+// 			$text = $event['message']['text'];
+// 			// Get replyToken
+// 			$replyToken = $event['replyToken'];
 
-			// Make a POST Request to Messaging API to reply to sender
-			//$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => $text,
-			];
+// 			// Make a POST Request to Messaging API to reply to sender
+// 			//$url = 'https://api.line.me/v2/bot/message/reply';
+// 			$data = [
+// 				'replyToken' => $replyToken,
+// 				'messages' => $text,
+// 			];
       
-      return $this->$data;
-}
-
+//       return $this->$data;
+// }
+echo "OK";
 ?>
