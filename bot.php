19a294b7149069881a8434923ad13241c757b1e7
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-17 10:59:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -101,15 +101,27 @@ if (!is_null($events['events'])) {
 					  "altText"=>"this is a buttons template",
 					  "template"=>[
 					      "type"=>"buttons",
-					      "text"=>$msg_check,
-					      "actions"=> [
+					      "thumbnailImageUrl"=>"https://example.com/bot/images/image.jpg",
+					      "title"=>"Menu",
+					      "text"=>"Please select",
+					      "actions"=>[
+						  [
+						    "type"=>"postback",
+						    "label"=>"Buy",
+						    "data"=>"action=buy&itemid=123"
+						  ],
+						  [
+						    "type"=>"postback",
+						    "label"=>"Add to cart",
+						    "data"=>"action=add&itemid=123"
+						  ],
 						  [
 						    "type"=>"uri",
-						    "label">"เพิ่มคำตอบ...",
-						    "uri"=>'http://202.28.37.32/smartcsmju/LineAPI/test_insert.php'
+						    "label"=>"View detail",
+						    "uri"=>"http://example.com/page/123"
 						  ]
 					      ]
-					]
+					  ]
 				];
 			}
 
