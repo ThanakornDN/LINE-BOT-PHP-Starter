cbe0176ed2af6691f3176b90dd6a0a5baa22c8a1
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-18 10:28:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,9 @@
 <?php
-
 $access_token = 'VlxSZTyumW3qJsUKMnKOTLdqRd7chFWFJARPb7ZB/n3Lzf/lntpuOwBiLNieMReH3aFrT4MoAEWCdFruNp/7VHg3RkM1ja3AUtYVlDabJUgo6wAKsQyrZVo9Vxq+/py7le7bLr6ZDSp6qQHy0RiI2gdB04t89/1O/w1cDnyilFU=';
 $msg="";
+$m_type="";
 $regs="";
+$ans_state=0;
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -14,11 +15,127 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			if($ans_state==1){
+				file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_insert.php?msg='.$text);
+			}
+			$ans_state=1;
 			
-		    //public function replyText($event);
-			$data = replyText($event);
+			$s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_ans.php?msg='.$text);
+			if(!eregi ( "S0",$s_ans, $regs )){
+					$messages = [
+						'type'=>'text',
+						'text'=>$s_ans
+					];
+			}else if((eregi ( "คือ", $text, $regs ))or(eregi ( "หมายถึง", $text, $regs ))or(eregi ( "=>", $text, $regs ))){
+				$text_msg = urlencode($text);
+				$sql_msg = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_insert.php?msg='.$text_msg);
+				$json_msg = json_decode($sql_msg, true);
+// 				$msg_split = explode($regs[0],$text);
+// 				$msg1=$msg_split[0]; 
+// 				$msg2=$msg_split[1];
+// 				$msg_check = "แน่ใจนะว่า ".$text." ?";
+// 				$test_insert = urlencode($msg1."|".$msg2);
+				
+// 				file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_insert.php?msg='.$test_insert);
+				
+				$messages = [
+					  "type"=>"template",
+					  "altText"=>"this is a confirm template",
+					  "template"=>[
+					      "type"=>"confirm",
+					      "text"=>$msg_check,
+					      "actions"=> [
+						  [
+						    "type"=>"message",
+						    "label"=>"Yes",
+						    "text"=>"yes"
+						  ],
+						  [
+						    "type"=>"message",
+						    "label"=>"No",
+						    "text"=>"no"
+						  ]
+					      ]
+					]
+				];
+			}else if((eregi ( "ตอบว่า", $text, $regs ))or(eregi ( "ตอบ", $text, $regs ))){
+				$msg_split = explode($regs[0],$text);
+				$msg1=$msg_split[0]; 
+				$msg2=$msg_split[1];
+				$msg_check = "แน่ใจนะว่า ".$text." ?";
+				$test_insert = urlencode($msg1."|".$msg2);
+				
+				file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_insert.php?msg='.$test_insert);
+				
+				$messages = [
+					  "type"=>"template",
+					  "altText"=>"this is a confirm template",
+					  "template"=>[
+					      "type"=>"confirm",
+					      "text"=>$msg_check,
+					      "actions"=> [
+						  [
+						    "type"=>"message",
+						    "label"=>"Yes",
+						    "text"=>"yes"
+						  ],
+						  [
+						    "type"=>"message",
+						    "label"=>"No",
+						    "text"=>"no"
+						  ]
+					      ]
+					]
+				];
+			}else if((eregi ( "ใช่", $text, $regs ))or(eregi ( "ตกลง", $text, $regs ))or(eregi ( "yes", $text, $regs ))or(eregi ( "ok", $text, $regs ))){
+				$messages = [
+					"id"=>"325708",
+					"type"=>"sticker",
+					"packageId"=>"1",
+					"stickerId"=>"1"
+				];
+			}else{
+				
+				$test_insert = "Hi|Hi";
+				$msg_check =$text."ต้องตอบว่าไงดี ?";
+				//file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_insert.php?msg='.$test_insert);
+				
+				$messages = [
+					  "type"=>"template",
+					  "altText"=>"this is a buttons template",
+					  "template"=>[
+					      "type"=>"buttons",
+					      "text"=>$msg_check,
+					      "actions"=>[
+						  [
+						    "type"=>"postback",
+						    "label"=>"Add",
+						    "data"=>"action=010"
+						  ],
+						  [
+						    "type"=>"uri",
+						    "label"=>"เพิ่มคำตอบ อื่น..",
+						    "uri"=>"http://202.28.37.32/smartcsmju/LineAPI/test_insert_user_msg.php"
+						  ]
+					      ]
+					  ]
+				];
+			}
+
+
+
+			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+				
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
@@ -36,5 +153,9 @@ if (!is_null($events['events'])) {
 	}
 }
 
+function add1()
+{
+file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/test_insert.php?msg=111');
+}
 echo "OK";
 
