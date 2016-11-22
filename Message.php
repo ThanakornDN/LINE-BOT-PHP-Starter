85d18c718814811fa36ab3698399c03542f4c1f3
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-22 14:07:07 +0700

    Update Message.php

--- Message.php
+++ Message.php
@@ -5,13 +5,14 @@ function find_ans($text) {
     $s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/update_frequency.php?msg='.$text);
     $msg_decode = json_decode($s_ans, true);
     $msg_stat = $msg_decode['data']['status'];
-    if($msg_stat=='S0'){
-        return '019';
-    }else if($msg_stat=='S1'){
-        return $s_ans;
-    }else{
-        return 'error';
-    }
+     return $s_ans;
+    //if($msg_stat=='S0'){
+    //    return '019';
+    //}else if($msg_stat=='S1'){
+    //    return $s_ans;
+    //}else{
+    //    return 'error';
+    //}
     
     
     
