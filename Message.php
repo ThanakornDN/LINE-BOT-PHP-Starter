1a069ee567578e22d521c3b5cb35ac2cdbddfd35
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-22 10:14:24 +0700

    Update Message.php

--- Message.php
+++ Message.php
@@ -1,8 +1,9 @@
 <?php
 
-function familyName() {
-    $aa="Hello";
-  return $aa;
+function find_ans($text) {
+    $s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/update_frequency.php?msg='.$text);
+    $ans="Hello";
+  return $ans;
 }
 
 ?>
