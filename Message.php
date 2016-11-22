2cbce5a7740a26e554630b8094ef94239ff9b39e
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-22 10:20:54 +0700

    Update Message.php

--- Message.php
+++ Message.php
@@ -2,7 +2,7 @@
 
 function find_ans($text) {
     $s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/update_frequency.php?msg='.$text);
-    $ans="Hello1";
+    $ans=$s_ans;
   return $ans;
 }
 
