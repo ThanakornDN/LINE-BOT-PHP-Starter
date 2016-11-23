77b615a1d6e2d53c4e5e0a904f04875b8e36b07a
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-23 13:19:45 +0700

    Update test_1.php

--- test_1.php
+++ test_1.php
@@ -26,14 +26,14 @@ if (!is_null($events['events'])) {
 			//if(eregi ("Hi",$text, $regs )){
 			
 			$s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/update_frequency.php?msg='.$text);			
-			//$msg_decode = json_decode($s_ans, true);
-				//foreach ($msg_decode['msg'] as $msg) {
-            			//	$msg_ans = $msg['text'];
-        			//}
-					$messages = [
-						'type'=>'text',
-						'text'=>$s_ans
-					];
+			$msg_decode = json_decode($s_ans, true);
+				foreach ($msg_decode['msg'] as $msg) {
+            				$msg_ans = $msg['text'];
+        			}
+					//$messages = [
+					//	'type'=>'text',
+					//	'text'=>$s_ans
+					//];
 			//}
 
 
@@ -42,8 +42,8 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
-				//'messages' => $msg_ans,
+				//'messages' => [$messages],
+				'messages' => $msg,
 			];
 				
 			$post = json_encode($data);
