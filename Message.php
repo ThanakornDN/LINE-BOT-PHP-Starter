974b483be21ec6398f6bd626a11069da71c9ff26
ToomTam <ThanakornDN@users.noreply.github.com>
2016-11-23 11:35:09 +0700

    Update Message.php

--- Message.php
+++ Message.php
@@ -8,7 +8,7 @@ function find_ans($text) {
     if($m_stat == 'S1'){
         //$msg_ans =$m_stat;
         foreach ($msg_decode['msg'] as $msg) {
-            $msg_ans = $msg['text'];
+            $msg_ans = $msg;
         }
     }else if($m_stat == 'S0'){
         $msg_ans =$m_stat;
