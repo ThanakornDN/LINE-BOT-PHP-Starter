5dce38f0ff1bb564c858b66e9c72e9d7f2bdc34c
ToomTam <ThanakornDN@users.noreply.github.com>
2016-12-06 10:41:24 +0700

    Update test_1.php

--- test_1.php
+++ test_1.php
@@ -24,28 +24,28 @@ if (!is_null($events['events'])) {
 			
 			
 			//$s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/update_frequency.php?msg='.$text);			
+			$msg_decode = json_decode($s_ans, true);
+				foreach ($msg_decode['msg'] as $msg) {
+            				$msg_type = $msg['type'];
+        			}
+			
+			if($msg_type=='text'){
+				$messages = $msg;
+			}else if($msg_type=='image'){
+				
+			}else if($msg_type=='video'){
+				
+			}else if($msg_type=='audio'){
+			
+			}else if($msg_type=='sticker'){
+			//
+			}
+			
 			//$msg_decode = json_decode($s_ans, true);
 			//	foreach ($msg_decode['msg'] as $msg) {
             		//		$msg_type = $msg['type'];
         		//	}
-			
-			//if($msg_type=='text'){
-			//	$messages = $msg;
-			//}else if($msg_type=='image'){
-			//	
-			//}else if($msg_type=='video'){
-			//	
-			//}else if($msg_type=='audio'){
-			
-			//}else if($msg_type=='sticker'){
-			//
-			//}
-			
-			$msg_decode = json_decode($s_ans, true);
-			//	foreach ($msg_decode['msg'] as $msg) {
-            		//		$msg_type = $msg['type'];
-        		//	}
-			$messages2 = $msg_decode['msg'];
+			//$messages2 = $msg_decode['msg'];
 					//$messages2 = [
 					//	'type'=>'text',
 					//	'text'=>$s_ans
@@ -57,7 +57,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages2],
+				'messages' => [$messages],
 				//'messages' => [$msg],
 			];
 				
