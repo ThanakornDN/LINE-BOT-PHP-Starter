d73aa21c31ea4f6d14881f5bb0303248d4819044
ToomTam <ThanakornDN@users.noreply.github.com>
2016-12-07 13:53:10 +0700

    Update test_1.php

--- test_1.php
+++ test_1.php
@@ -171,6 +171,35 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+		}else if ($event['type'] == 'postback' ) {
+
+			$text = $event['postback']['data'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];	
+			
+			$messages = [
+				'type'=>'text',
+				'text'=>$text
+			];	
+			
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+				
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+		
 		}
 	}
 }
