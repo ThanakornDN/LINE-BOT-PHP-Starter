7ec023f6bf695e8ef1f5461bc50a4d716f65463b
ToomTam <ThanakornDN@users.noreply.github.com>
2016-12-09 14:17:17 +0700

    Update test_1.php

--- test_1.php
+++ test_1.php
@@ -236,7 +236,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $str[0],
 			];
 				
 			$post = json_encode($data);
