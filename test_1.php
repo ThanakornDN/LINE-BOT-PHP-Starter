b0bb3dc5a504c406d70b2cce4913d579dfc8adec
ToomTam <ThanakornDN@users.noreply.github.com>
2016-12-09 15:40:49 +0700

    Update test_1.php

--- test_1.php
+++ test_1.php
@@ -181,7 +181,7 @@ if (!is_null($events['events'])) {
 			$insertMSG = $str[1]."|".$str[2];
 
 			if($str[0] == "update"){
-				$result = $s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/update_frequency.php?msg='.$text);
+				$result = $s_ans = file_get_contents('http://202.28.37.32/smartcsmju/LineAPI/update_frequency.php?msg='.$str[1]);
 				$messages = [
 					 "type"=> "sticker",
 					 "packageId"=> "2",
