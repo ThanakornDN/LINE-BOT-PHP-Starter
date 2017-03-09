3e735a5e547b53d3ace70a4a79db7779ddb61a4e
ToomTam <ThanakornDN@users.noreply.github.com>
2017-03-09 16:05:09 +0700

    Update test_mqtt.php

--- test_mqtt.php
+++ test_mqtt.php
@@ -7,46 +7,39 @@ var host= "m12.cloudmqtt.com";
 var port = 34115;
 var user = "tjrehruv";
 var pass = "FNBY4LmV_evX";
- 
 
- 
- 
-client = new Paho.MQTT.Client(host, Number(port), "clientId60");
- 
+client = new Paho.MQTT.Client(host, Number(port), "clientId62");
+
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
- 
+
 // connect the client
 client.connect({onSuccess:onConnect,userName : user, password : pass, useSSL: true});
- 
+
+
+
 // called when the client connects
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
-  client.subscribe("TEST/MQTT/HEROKU/1");
-//   message = new Paho.MQTT.Message("Hello Sv"); //message pub
-  message = new Paho.MQTT.Message("<?php echo "f"; ?>"); //message pub
+  client.subscribe("TEST/MQTT/HEROKU/1"); //Toppic sub
+  //message = new Paho.MQTT.Message("Hello MJU"); //message pub
+  message = new Paho.MQTT.Message("<?php echo $msg_mqtt; ?>"); //message pub
   message.destinationName = "TEST/MQTT/HEROKU/1"; //Toppic pub
   client.send(message);
 }
- 
+
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
     console.log("onConnectionLost:"+responseObject.errorMessage);
   }
 }
- 
+
 // called when a message arrives
 function onMessageArrived(message) {
   console.log("onMessageArrived:"+message.payloadString);
+ // alert(message);
 }
- 
-function pub($text) {
-  console.log("<?php echo $content; ?>");
-  message = new Paho.MQTT.Message("<?php echo $text; ?>");
-  message.destinationName = "INTNIN_LAB/CHAT_BOT";
-  client.send(message); 
-} 
 </script>
